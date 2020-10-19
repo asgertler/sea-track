ac3e64598370c6fea539b8827dadc92bd820c7b5
Aaron Gertler <aaron.gertler@gmail.com>
2020-10-19 11:54:09 -0500

    successfully launched react host

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Route, Redirect } from "react-router-dom"
 import { ApplicationViews } from "./ApplicationViews"
 import { NavBar } from "./nav/NavBar"
-import 'semantic-ui-css/semantic.min.css'
 import "./Dashboard.css"
 
 export const Dashboard = () => (
