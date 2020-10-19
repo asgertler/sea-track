4094ae9bc0811e163cd4d1533b0ed0d572893f85
Aaron Gertler <aaron.gertler@gmail.com>
2020-10-19 11:50:23 -0500

    added base structure to dashboard

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Route, Redirect } from "react-router-dom"
+import { ApplicationViews } from "./ApplicationViews"
+import { NavBar } from "./nav/NavBar"
+import 'semantic-ui-css/semantic.min.css'
+import "./Dashboard.css"
+
+export const Dashboard = () => (
+    <>
+        <h2>This is the home page</h2>
+    </>
+)
---
+++ src/components/aquarium/Aquarium.js
@@ -0,0 +1,10 @@
+import React from "react"
+import 'semantic-ui-css/semantic.min.css'
+import "./Aquarium.css"
+
+export const Aquarium = () => (
+    <>
+        <h2>Aquarium View</h2>
+        <p>Aquarium will display here</p>
+    </>
+)
