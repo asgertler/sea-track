e8f6f8c5f4e46130bbd639daf3c139355ffcce9e
Aaron Gertler <aaron.gertler@gmail.com>
2020-10-29 10:58:07 -0500

    created AquariumTask

--- src/components/aquariumtasks/AquariumTask.js
+++ src/components/aquariumtasks/AquariumTask.js
@@ -1,1 +1,15 @@
 import React, { useContext, useState } from "react"
+import { useHistory } from "react-router-dom"
+import { AquariumTasksContext } from "./AquariumTasksProvider"
+import { Checkbox } from "semantic-ui-react"
+import "./AquariumTasks.css"
+
+export const AquariumTask = ({ task }) => {
+    const { deleteAquariumTask } = useContext(AquariumTasksContext)
+
+    return (
+        <div className="aquariumTask">
+            <Checkbox label={task.task} />
+        </div>
+    )
+}
