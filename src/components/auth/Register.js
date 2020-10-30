8259c09f70cae69c7a4bb5a5dd65245718c2f00c
Aaron Gertler <aaron.gertler@gmail.com>
2020-10-29 19:18:19 -0500

    added video to registration

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { useRef } from "react"
 import { useHistory } from "react-router-dom"
+import video from "../../video/seaTrack_vid.mp4"
 import "./Login.css"
 
 export const Register = (props) => {
@@ -61,45 +62,51 @@ export const Register = (props) => {
     }
 
     return (
-        <main style={{ textAlign: "center" }}>
+        <>
+            <main style={{ textAlign: "center" }}>
 
-            <dialog className="dialog dialog--password" ref={conflictDialog}>
-                <div>Account with that email address already exists</div>
-                <button className="button--close" onClick={e => conflictDialog.current.close()}>Close</button>
-            </dialog>
+                <dialog className="dialog dialog--password" ref={conflictDialog}>
+                    <div>Account with that email address already exists</div>
+                    <button className="button--close" onClick={e => conflictDialog.current.close()}>Close</button>
+                </dialog>
 
-            <form className="form--login" onSubmit={handleRegister}>
-                <h1 className="h3 mb-3 font-weight-normal">Join Sea Track</h1>
+                <form className="form--login" onSubmit={handleRegister}>
+                    <h1 className="h3 mb-3 font-weight-normal">Join Sea Track</h1>
 
-                <fieldset>
-                    <label htmlFor="firstName"> First Name </label>
-                    <input ref={firstName} type="text" name="firstName" className="form-control" placeholder="First name" required autoFocus />
+                    <fieldset>
+                        <label htmlFor="firstName"> First Name </label>
+                        <input ref={firstName} type="text" name="firstName" className="form-control" placeholder="First name" required autoFocus />
 
-                    <label htmlFor="lastName"> Last Name </label>
-                    <input ref={lastName} type="text" name="lastName" className="form-control" placeholder="Last name" required />
+                        <label htmlFor="lastName"> Last Name </label>
+                        <input ref={lastName} type="text" name="lastName" className="form-control" placeholder="Last name" required />
 
-                    <label htmlFor="inputEmail"> Email address </label>
-                    <input ref={email} type="email" name="email" className="form-control" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" placeholder="Email address" required />
+                        <label htmlFor="inputEmail"> Email address </label>
+                        <input ref={email} type="email" name="email" className="form-control" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" placeholder="Email address" required />
 
-                    <label htmlFor="inputCity"> City </label>
-                    <input ref={city} type="text" name="city" className="form-control" placeholder="City" required />
+                        <label htmlFor="inputCity"> City </label>
+                        <input ref={city} type="text" name="city" className="form-control" placeholder="City" required />
 
-                    <label htmlFor="selectState"> State </label>
-                    <select ref={state}>
-                        {stateList.map(state => {
-                            return <option key={state} value={state}>{state}</option>
-                        })}
-                    </select>
+                        <label htmlFor="selectState"> State </label>
+                        <select ref={state}>
+                            {stateList.map(state => {
+                                return <option key={state} value={state}>{state}</option>
+                            })}
+                        </select>
 
-                    <input ref={zip} type="text" name="zip" className="form-control" pattern="[0-9]{5}" maxLength="5" placeholder="Zipcode" required />
+                        <input ref={zip} type="text" name="zip" className="form-control" pattern="[0-9]{5}" maxLength="5" placeholder="Zipcode" required />
 
-                    <button type="button" onClick={() => {
-                        history.push("/login")
-                    }}> Cancel </button>
-                    <button type="submit"> Register </button>
-                </fieldset>
-            </form>
-        </main>
+                        <button type="button" onClick={() => {
+                            history.push("/login")
+                        }}> Cancel </button>
+                        <button type="submit"> Register </button>
+                    </fieldset>
+                </form>
+            </main>
+
+            <video className="videoBg" autoPlay loop muted>
+                <source src={video} type="video/mp4" />
+            </video>
+        </>
     )
 }
 
