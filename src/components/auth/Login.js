b09da52f8a528f629bb32b04c49ce7c048347715
Aaron Gertler <aaron.gertler@gmail.com>
2020-10-29 19:16:46 -0500

    added video background to login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -2,6 +2,7 @@ import React, { useRef } from "react"
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom"
 import { Container, Form } from "semantic-ui-react"
+import video from "../../video/seaTrack_vid.mp4"
 import "./Login.css"
 
 
@@ -31,36 +32,42 @@ export const Login = props => {
     }
 
     return (
-        <main className="container--login">
-            <dialog className="dialog dialog--auth" ref={existDialog}>
-                <div>User does not exist</div>
-                <button className="button--close" onClick={e => existDialog.current.close()}>Close</button>
-            </dialog>
+        <>
+            <main className="container--login">
+                <dialog className="dialog dialog--auth" ref={existDialog}>
+                    <div>User does not exist</div>
+                    <button className="button--close" onClick={e => existDialog.current.close()}>Close</button>
+                </dialog>
 
-            <Container className="form--container" text={true}>
-                <Form className="form--login" onSubmit={handleLogin}>
-                    <h1>Sea Track</h1>
-                    <h2>Login</h2>
+                <Container className="form--container" text={true}>
+                    <Form className="form--login" onSubmit={handleLogin}>
+                        <h1>Sea Track</h1>
+                        <h2>Login</h2>
 
-                    <Form.Field className="login--field">
-                        <label> Email </label>
-                        <input ref={email} type="email"
-                            id="email"
-                            className="email--input"
-                            placeholder="Email address"
-                            required autoFocus />
+                        <Form.Field className="login--field">
+                            <label> Email </label>
+                            <input ref={email} type="email"
+                                id="email"
+                                className="email--input"
+                                placeholder="Email address"
+                                required autoFocus />
 
-                        <Form.Button primary className="button--submit" type="submit">
-                            Sign in
+                            <Form.Button primary className="button--submit" type="submit">
+                                Sign in
                         </Form.Button>
-                    </Form.Field>
-                </Form>
+                        </Form.Field>
+                    </Form>
 
-                <div className="link--register">
-                    <Link to="/register">Not a member yet?</Link>
-                </div>
-            </Container>
-        </main>
+                    <div className="link--register">
+                        <Link to="/register">Not a member yet?</Link>
+                    </div>
+                </Container>
+            </main>
+
+            <video className="videoBg" autoPlay loop muted>
+                <source src={video} type="video/mp4" />
+            </video>
+        </>
     )
 }
 
